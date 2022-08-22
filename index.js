c2cf0dc24aa278abaa7b8f093a74defbc3c9bda3
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-22 13:52:06 +0200

    ajout de commentaires
    amélioration de la compréhension du code pour un programmeur externe

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+// importations
 const Numbers = require('./src/numbers');
 const { exit, rawListeners } = require('process');
 const readline = require('readline');
 const { log } = require('console');
 
+// le line reader pour les entrés utilisateur
 const lineReader = readline.createInterface(process.stdin, process.stdout);
-
-
-const numbers = Numbers.numbers;
-
+// instantiation d'un nouveau nombre
+let numbers = Numbers.numbers;
+// on définit la question
 lineReader.setPrompt(numbers.question());
+// on affiche la question
 lineReader.prompt();
+// on demande une entrée utilisateur
 lineReader.on('line', (saisie) => {
-    numbers.compare(saisie);
+    // on envoie l'entrée a la fonction de comparaison
+    // et on affiche le résultat
+    console.log(numbers.compare(saisie));
+    // si l'état de l'instance à changée en ...
     if (numbers.success) {
+        // on ferme le lineReader
         lineReader.close()
+        // et on quitte le programme
         exit(0);
+        // on pourrait pour créer une boucle 
+        /**
+         *  placer tout le lineReader.on ... dans une fonction
+         * et ici polacer un nouveau prompt utilisateur pour lui 
+         * demander s'il souhaite continuer 
+         * - le cas échéant la fonction s'appellerais elle-même 
+         *  -à l'inverse  le close() et le exit(0) seraient éxecutés.
+         */
+
     }
 });
-//numbers.success = true;
 
 
--- src/numbers.js
+++ src/numbers.js
@@ -1,47 +1,71 @@
+/**
+ * claas numbers 
+ * génére un nombre entre 0 et 100
+ * puis compare celui-ci avec un nombre qui lui est donné en entrée
+ * incrémente le nombre d'appel au comparateur
+ * et répond par un message indiquant le resultat de la comparaison
+ * 
+ */
 class Numbers {
 
-
+    /** texte si le nombre donnée est trop  petit*/
     static PLUS_GRAND = `Le nombre est plus grand`;
+    /** texte si le nombre donnée est trop grand */
     static PLUS_PETIT = `Le nombre est plus petit`;
+    /** texte pour le titre du programme */
     static TITRE = `Trouvez le bon nombre`;
+    /**texte pour la prompt utilisateur */
     static PROMPT = 'Donnez un nombre ';
 
     constructor() {
         console.log(Numbers.TITRE);
         this.success = false;
         this.coups = 0;
+        // on génére un nombre entre 0 et 100
         this.nombre = parseInt(Math.random() * 100);
-
-
-
     }
 
+    /** compare this.nombre avec l'entrée
+    * incrémente le nombre d'appel au comparateur
+    * répond par un message d'état   
+    * @param {*} pnombre  une entrée
+    * @returns un message indiquant le resultat de la comparaison
+    */
     compare(pnombre) {
         let nombre = parseInt(pnombre);
 
         if (nombre < this.nombre) {
             this.coups += 1;
-            console.log(Numbers.PLUS_GRAND);
+            return Numbers.PLUS_GRAND;
         }
         else if (nombre > this.nombre) {
             this.coups += 1;
-            console.log(Numbers.PLUS_PETIT);
+            return Numbers.PLUS_PETIT;
 
         }
         else {
             this.success = true;
             this.coups += 1;
-            console.log(this.trouve());
+            return this.trouve();
         }
     }
-
+    /**
+     *
+     * @returns un message de question
+     */
     question() {
         return Numbers.PROMPT;
     }
+    /**
+     * utilisé dans le cas d'une comparaison  gagnante 
+     *  
+     * @returns le message Trouvé mis à jour avec les propriétées de l'instance
+     */
     trouve() {
-        return `vous avez trouvé en ${this.coups} coups, c'était le nombre : ${this.nombre}`;
+        return `vous avez trouvé en ${this.coups} coups.`;
     }
 
 }
-
+// donc il faut effectivement  donner un nom à l'export
+// export.constructeur pour exporter un constructeur
 exports.numbers = new Numbers();
